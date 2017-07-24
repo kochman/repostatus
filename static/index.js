aa82aadadbf2ad3b12f0ce0f752f5fe95394acd2
Sidney Kochman <sidney@kochman.org>
2017-07-23 23:29:33 -0400

    Fix index page centering on small width screens

--- static/index.js
+++ static/index.js
@@ -18,18 +18,16 @@ Vue.component('index', {
 	template: `
 		<section class="jumbotron text-center">
 			<div class="container">
-				<div class="row justify-content-md-center centered">
-					<h1 class="jumbotron-heading">RepoStatus</h1>
-					<p class="lead text-muted">Get the status of any public GitHub repository's branches.</p>
-					<form class="form-inline text-center" v-on:submit.prevent="getStatus()">
-						<div class="input-group">
-							<input type="text" v-model="orgRepo" autocorrect="off" autocapitalize="off" spellcheck="false" class="form-control form-control-lg">
-							<div class="input-group-btn">
-								<button class="btn btn-lg btn-primary">Get status</button>
-							</div>
+				<h1 class="jumbotron-heading">RepoStatus</h1>
+				<p class="lead text-muted">Get the status of any public GitHub repository's branches.</p>
+				<form class="form-inline justify-content-center" v-on:submit.prevent="getStatus()">
+					<div class="input-group">
+						<input type="text" v-model="orgRepo" autocorrect="off" autocapitalize="off" spellcheck="false" class="form-control form-control-lg">
+						<div class="input-group-btn">
+							<button class="btn btn-lg btn-primary">Get status</button>
 						</div>
-					</form>
-				</div>
+					</div>
+				</form>
 			</div>
 		</section>
 	`
